68b6415bb89577ad20fde285eb2022fa4b00198b
bonkalonk <s.ward.d.9+github@gmail.com>
2018-06-29 11:12:22 +0100

    Deck work & c++ language changes

--- Escalation/Inc/Deck.h
+++ Escalation/Inc/Deck.h
@@ -1,9 +1,120 @@
 #pragma once
 
+#include <assert.h>
 #include <memory>
-#include <set>
-#include <vector>
+#include <optional>
+#include <type_traits>
 
+struct CardBase
+{
+	CardBase() : _cardId( _nextCardId++ )
+	{
+		assert( _cardId != _InvalidId );
+	}
+	CardBase( CardBase&& other ) : _cardId( other._cardId )
+	{
+		assert( _cardId != _InvalidId );
+	}
+
+	CardBase( const CardBase& ) = delete;
+	void operator = ( const CardBase& ) = delete;
+	void operator = ( const CardBase&& ) = delete;
+
+	int GetCardId() const { return _cardId; }
+
+private:
+	inline static int _nextCardId = 0;
+	static const int _InvalidId = -1;
+
+	const int _cardId = _InvalidId;
+};
+
+
+template < typename CardType >
+struct DeckCore
+{
+	static_assert( std::is_base_of_v<CardBase, CardType>, "DeckCore can only contain Cards. Please inherit from CardBase to use." );
+
+	struct CardRef
+	{
+		CardRef( CardType&& card, DeckCore& deck ) : _card( std::move( card ) ), _deck( deck ) {}
+		~CardRef()
+		{
+			_deck.DiscardCard( /*_card*/ );
+		}
+
+		const CardType& GetCard() const { return _card; }
+
+	protected:
+		CardType _card;
+		DeckCore& _deck;
+	};
+
+	using Card = std::unique_ptr< CardRef >;
+
+	void AddNewCard()
+	{}
+
+	void DiscardCard()
+	{}
+
+	std::optional<Card> DrawCard()
+	{
+		return {};
+	}
+
+protected:
+	// TODO : Card storage
+};
+
+
+namespace DeckFunctions
+{
+	template < typename Derived >
+	struct DrawCardFunc
+	{
+		auto DrawCard()
+		{
+			return static_cast< Derived* >( this )->GetCore().DrawCard();
+		}
+	};
+
+	template < typename Derived >
+	struct DiscardCardFunc
+	{
+		void DiscardCard()
+		{
+			static_cast< Derived* >( this )->GetCore().DiscardCard();
+		}
+	};
+}
+
+template < typename CardType, template< typename > typename... DeckFuncs >
+struct DeckHost : DeckFuncs< DeckHost< CardType, DeckFuncs... > >...
+{
+	DeckCore< CardType >& GetCore()
+	{
+		return _deck;
+	}
+
+protected:
+	DeckCore< CardType > _deck;
+};
+
+template < typename CardType, template< typename > typename... DeckFuncs >
+struct Deck : DeckHost< CardType, DeckFuncs... >
+{
+	using Base = DeckHost< CardType, DeckFuncs... >;
+
+private:
+	DeckCore< CardType >& GetCore()
+	{
+		return Base::GetCore();
+	}
+};
+
+
+/*
 struct Deck
 {
 protected:
@@ -71,6 +182,6 @@ protected:
 	PileType _drawPile;
 	PileType _discardPile;
 	std::vector<int> _uniqueIds;
-	
+
 	void FillDrawPile();
-};
+};*/
